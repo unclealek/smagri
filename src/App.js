d5d4b2decaec74a544ddd839b3ebe67d3da3e56d
uncluealek <kelvin.aliche@gmail.com>
2024-05-14 22:04:48 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -53,9 +53,25 @@ const App = () => {
                     <Route path="/ecommerce" element="ECommerce" />
                   
                     {/* Pages */}
+                    <Route path="/orders" element="Employees"/>
                     <Route path="/employees" element="Employees"/>
                     <Route path="/customers" element="Customers"/>
-                    
+                    {/* Apps */}
+                    <Route path="/kanban" element="Kandban"/>
+                    <Route path="/editor" element="Editor"  />
+                    <Route path="/calendar" element="Calendar"  />
+                    <Route path="/color-picker" element= "ColorPicker" />
+
+                {/* charts  */}
+                <Route path="/line" element="Line" />
+                <Route path="/area" element="Area" />
+                <Route path="/bar" element="Bar" />
+                <Route path="/pie" element="Pie" />
+                <Route path="/financial" element="Financial" />
+                <Route path="/color-mapping" element="ColorMapping" />
+                <Route path="/pyramid" element="Pyramid" />
+                <Route path="/stacked" element="Stacked" />
+
                 </Routes>
             </div>
 
